a5642a343df3dbf9a68b69702b5b0cfee4bee6bb
Vinicio <viniciobb@users.noreply.github.com>
2020-09-05 10:50:24 -0300

    first commit

---
+++ lib/SomethingWrong/SomethingWrong.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SomethingWrong extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Erro'),
+      ),
+
+    );
+  }
+}
