e97a60044365242867690637fbe520745818dc8d
Vinicio <viniciobb@users.noreply.github.com>
2020-09-05 21:28:21 -0300

    flutter cookbook

---
+++ lib/Mains - teste/Main fading Container.dart
@@ -0,0 +1,55 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MaterialApp(
+    home: MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  bool _visible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("container fading"),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 0.0 : 1.0,
+          duration: Duration(milliseconds: 2000),
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: 'Toogle Opacity',
+        child: Icon(Icons.flip),
+      ),
+
+    );
+  }
+
+}
+
+
+
+
+
